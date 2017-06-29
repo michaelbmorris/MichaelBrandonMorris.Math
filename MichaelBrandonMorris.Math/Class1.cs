fd5e239b804fcb62ca13931bc3e9f011cf06a652
Michael Brandon Morris <brandon@michaelbmorris.com>
2017-06-29 11:19:57 -0400

    Add project files.

---
+++ MichaelBrandonMorris.Math/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MichaelBrandonMorris.Math
+{
+    public class Class1
+    {
+    }
+}
