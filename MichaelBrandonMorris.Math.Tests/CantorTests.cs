8ff9b0d8a12f603ed74c9abd6feac2e89fe93679
Michael Brandon Morris <brandon@michaelbmorris.com>
2017-06-29 12:41:49 -0400

    Created class for Cantor pairing. Created tests for Cantor pairing.

---
+++ MichaelBrandonMorris.Math.Tests/CantorTests.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MichaelBrandonMorris.Math.Tests
+{
+    /// <summary>
+    ///     Class CantorTests.
+    /// </summary>
+    /// TODO Edit XML Comment Template for CantorTests
+    [TestClass]
+    public class CantorTests
+    {
+        /// <summary>
+        ///     Determines whether this instance [can invert pair].
+        /// </summary>
+        /// TODO Edit XML Comment Template for CanInvertPair
+        [TestMethod]
+        public void CanInvertPair()
+        {
+            (int x, int y) = Cantor.Inverse(8);
+            Assert.AreEqual(x, 1);
+            Assert.AreEqual(y, 2);
+        }
+
+        /// <summary>
+        ///     Determines whether this instance [can invert tuple].
+        /// </summary>
+        /// TODO Edit XML Comment Template for CanInvertTuple
+        [TestMethod]
+        public void CanInvertTuple()
+        {
+            var ints = Cantor.Inverse(69, 3);
+            Assert.AreEqual(ints.Length, 3);
+            Assert.AreEqual(ints[0], 1);
+            Assert.AreEqual(ints[1], 2);
+            Assert.AreEqual(ints[2], 3);
+        }
+
+        /// <summary>
+        ///     Determines whether this instance can pair.
+        /// </summary>
+        /// TODO Edit XML Comment Template for CanPair
+        [TestMethod]
+        public void CanPair()
+        {
+            var z = Cantor.Pair(1, 2);
+            Assert.AreEqual(z, 8);
+        }
+
+        /// <summary>
+        ///     Determines whether this instance can tuple.
+        /// </summary>
+        /// TODO Edit XML Comment Template for CanTuple
+        [TestMethod]
+        public void CanTuple()
+        {
+            var z = Cantor.Tuple(1, 2, 3);
+            Assert.AreEqual(z, 69);
+        }
+    }
+}
---
+++ MichaelBrandonMorris.Math/Cantor.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using MichaelBrandonMorris.Extensions.CollectionExtensions;
+
+namespace MichaelBrandonMorris.Math
+{
+    /// <summary>
+    ///     Class Cantor.
+    /// </summary>
+    /// TODO Edit XML Comment Template for Cantor
+    public static class Cantor
+    {
+        /// <summary>
+        ///     Inverses the specified z.
+        /// </summary>
+        /// <param name="z">The z.</param>
+        /// <returns>System.Int32.</returns>
+        /// TODO Edit XML Comment Template for Inverse
+        public static (int x, int y) Inverse(int z)
+        {
+            var w = (int) System.Math.Floor(
+                (System.Math.Sqrt(8 * z + 1) - 1) / 2);
+            var t = (w * w + w) / 2;
+            var y = z - t;
+            var x = w - y;
+            return (x, y);
+        }
+
+        /// <summary>
+        ///     Inverses the specified z.
+        /// </summary>
+        /// <param name="z">The z.</param>
+        /// <param name="n">The n.</param>
+        /// <returns>System.Int32[].</returns>
+        /// <exception cref="ArgumentException">
+        ///     Cantor inverse function
+        ///     requires n to be at least two.
+        /// </exception>
+        /// TODO Edit XML Comment Template for Inverse
+        public static int[] Inverse(int z, int n)
+        {
+            if (n < 2)
+            {
+                throw new ArgumentException(
+                    "Cantor inverse function requires n to be at least two.");
+            }
+
+            return Inverse(z, n, new int[n]);
+        }
+
+        /// <summary>
+        ///     Pairs the specified x.
+        /// </summary>
+        /// <param name="x">The x.</param>
+        /// <param name="y">The y.</param>
+        /// <returns>System.Int32.</returns>
+        /// TODO Edit XML Comment Template for Pair
+        public static int Pair(int x, int y)
+        {
+            return (x + y) * (x + y + 1) / 2 + y;
+        }
+
+        /// <summary>
+        ///     Tuples the specified ints.
+        /// </summary>
+        /// <param name="ints">The ints.</param>
+        /// <returns>System.Int32.</returns>
+        /// <exception cref="ArgumentException">
+        ///     Cantor tuple function
+        ///     requires at least two arguments.
+        /// </exception>
+        /// TODO Edit XML Comment Template for Tuple
+        public static int Tuple(params int[] ints)
+        {
+            if (ints.Length < 2)
+            {
+                throw new ArgumentException(
+                    "Cantor tuple function requires at least two arguments.");
+            }
+
+            return Tuple(ints[0], ints[1], ints.Skip(2));
+        }
+
+        /// <summary>
+        ///     Inverses the specified z.
+        /// </summary>
+        /// <param name="z">The z.</param>
+        /// <param name="n">The n.</param>
+        /// <param name="ints">The ints.</param>
+        /// <returns>System.Int32[].</returns>
+        /// TODO Edit XML Comment Template for Inverse
+        private static int[] Inverse(int z, int n, int[] ints)
+        {
+            while (true)
+            {
+                (int x, int y) = Inverse(z);
+
+                if (n == 2)
+                {
+                    ints[0] = x;
+                    ints[1] = y;
+
+                    return ints;
+                }
+
+                n--;
+                ints[n] = y;
+                z = x;
+            }
+        }
+
+        /// <summary>
+        ///     Tuples the specified x.
+        /// </summary>
+        /// <param name="x">The x.</param>
+        /// <param name="y">The y.</param>
+        /// <param name="ints">The ints.</param>
+        /// <returns>System.Int32.</returns>
+        /// TODO Edit XML Comment Template for Tuple
+        private static int Tuple(int x, int y, int[] ints)
+        {
+            while (true)
+            {
+                var pair = Pair(x, y);
+
+                if (ints.Length == 0)
+                {
+                    return pair;
+                }
+
+                x = pair;
+                y = ints[0];
+                ints = ints.Skip(1);
+            }
+        }
+    }
+}
